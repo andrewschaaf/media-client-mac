a78c1d4683ae0ec01e114ef57d76ba73c13f8d96
Andrew Schaaf <andrew@andrewschaaf.com>
2011-09-10 15:02:17 -0400

    stub Cocoa app (document-based)

---
+++ MediaClient/ItemDocument.h
@@ -0,0 +1,14 @@
+//
+//  ItemDocument.h
+//  MediaClient
+//
+//  Created by Andrew Schaaf on 9/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ItemDocument : NSDocument {
+}
+
+@end
---
+++ MediaClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MediaClient
+//
+//  Created by Andrew Schaaf on 9/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
